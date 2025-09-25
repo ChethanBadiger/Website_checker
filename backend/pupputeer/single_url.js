6219807fc485c9eb3c3aabc57f1f47ee6cab3386
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-25 19:00:54 +0530

    puppteer single link working version 1.0.0

---
+++ backend/pupputeer/single_url.js
@@ -0,0 +1,88 @@
+import { log } from "console";
+import fs from "fs";
+import path from "path";
+import puppeteer from "puppeteer";
+
+const replace_space = (name) => name.replace(/[:\/\\?%*:|"<>]/g, "_");
+
+async function checkSite(url, outputDir) {
+    const res = {
+        url,
+        errors: [],
+        anomaly: false,
+        finalUrl: null,
+        screenshotPath: null,
+        log: null 
+    };
+
+    const browser = await puppeteer.launch({
+        headless: 'true',
+        args: ['--no-sandbox']
+    });
+
+    const page = await browser.newPage();
+
+    try{
+        const response = await page.goto(url, {
+            timeout: 40000,
+            waitUntil: 'networkidle2'
+        });
+
+        res.finalUrl = page.url();
+
+        let redirectCount = 0;
+        if (response && response.request()) {
+            let req = response.request();
+            while (req.redirectChain().length > redirectCount){
+                redirectCount = req.redirectChain().length;
+            }
+        }
+        if (redirectCount > 3) res.errors.push('too many redirects');
+
+        const content = await page.content();
+
+        if(/(SQL syntax|mysqli|PDOException|ORA-|error establishing a database connection|database error)/i.test(content)) {
+            res.errors.push('database error have been dectected');
+        }
+
+        if(/(casino|betting|poker|gambling|free spins|porn|xxx|sex)/i.test(content)) {
+            res.anomaly = true;
+            res.errors.push('adult/gambling content');
+        }
+
+        if(/(captcha|verify you are human|are you a robot|bot checker|cloudflare|hcaptcha|recaptcha)/i.test(content)) {
+            res.anomaly = true;
+            res.errors.push('captcha');
+        }
+
+        const fname = `${replace_space(url)}.png`;
+        const fullPath = path.join(outputDir, fname);
+        await fs.promises.mkdir(outputDir, { recursive: true });
+        await page.screenshot({ path: fullPath, fullPage: true });
+        res.screenshotPath = fullPath;
+    } catch (error) {
+        const msg = error.message || '';
+        if (/SSL|CERT|certificate/i.test(msg)) {
+            res.errors.push('ssl error');
+        } else if (/Timeout|Navigation|ENOTFOUND|ERR_NAME_NOT_RESOLVED/i.test(msg)) {
+            res.errors.push('site unreachable');
+        } else {
+            res.errors.push('other: ' + msg.slice(0, 200));
+        }
+    }
+    finally {
+        await browser.close();
+        const now = new Date();
+        const date = now.toISOString().split("T")[0];      
+        const time = now.toISOString().split("T")[1].split("Z")[0]; 
+
+        res.log = `Check finished at: ${date} ${time}`;
+    }
+
+    return res;
+}
+
+(async () => {
+    const result = await checkSite('https://jperm.net', './results');
+    console.log(result);
+})();
