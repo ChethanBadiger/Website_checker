fc1463df2f0e04f4dc6d68d6fa74dbefff3c5df6
ChethanBadiger <chethanbadiger245@gmail.com>
2025-09-26 19:45:22 +0530

    small change

--- backend/sqliteDB.js
+++ backend/sqliteDB.js
@@ -1,9 +1,7 @@
 const Database = require("better-sqlite3");
 
-// Open or create the database file
 const db = new Database("./urls.db");
 
-// Create table if it doesn’t exist
 db.prepare(
   `
   CREATE TABLE IF NOT EXISTS urls (
